f7454718e4edb479d263cdae7696f3f98c7a33e4
nicolaebalan7 <nicolae.balan@student.tuiasi.ro>
2026-04-20 20:12:09 +0300

    Add files via upload

---
+++ Laborator 3/pb1/mergeSort.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include<iostream>
+using namespace std;
+
+void mergeSort(int a[], int p, int q, int t[]);
+void interclasare(int a[], int p, int q, int m, int t[]);
+void citireVector(int a[], int n);
+void afisareVector(int a[], int n);
