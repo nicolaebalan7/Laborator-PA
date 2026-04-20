9fe119e9cf2d5e69782d4c067b90d401409306f5
nicolaebalan7 <nicolae.balan@student.tuiasi.ro>
2026-04-20 20:15:39 +0300

    Add files via upload

---
+++ Laborator 4/batcherSort.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include<iostream>
+using namespace std;;
+
+void batcherSort(int a[], int i, int d, int s);
+void secventaBitona(int a[], int i, int d, int s);
+void compara_si_schimba(int &a, int &b, int s);
+void citireVector(int a[], int n);
+void afisareVector(int a[], int n);
